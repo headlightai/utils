e9ef3e8f14be61398c50087d9084ff8c291ecd18
ashbabu <ash.babu@headlight.ai>
2025-10-28 17:27:32 +0000

    custom pcl point type
    Signed-off-by: ashbabu <ash.babu@headlight.ai>

---
+++ include/utils/custom_pcl_pointxyzrgbi.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#define PCL_NO_PRECOMPILE
+#include <pcl/point_types.h>
+#include <pcl/register_point_struct.h> // POINT_CLOUD_REGISTER_POINT_STRUCT
+
+/* 
+    The realsense camera pointclouds are of type PointXYZRGB and 
+    the lidar pointclouds are of type PointXYZI. To fuse them together, 
+    we define a custom point type PointXYZRGBI which has fields for 
+    x,y,z,rgb and intensity.
+*/
+namespace pcl {
+struct EIGEN_ALIGN16 PointXYZRGBI
+{
+  PCL_ADD_POINT4D;                 // adds float x,y,z and padding (data[4])
+  PCL_ADD_RGB                     
+  float intensity;
+
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+} EIGEN_ALIGN16;
+}
+// Register the struct with PCL (names in parenthesis must match member names)
+POINT_CLOUD_REGISTER_POINT_STRUCT(
+  pcl::PointXYZRGBI,
+  (float, x, x)
+  (float, y, y)
+  (float, z, z)
+  (float, rgb, rgb)
+  (float, intensity, intensity)
+)
+
+// #define PCL_INSTANTIATE_KEYWORD(PointXYZRGBI)
